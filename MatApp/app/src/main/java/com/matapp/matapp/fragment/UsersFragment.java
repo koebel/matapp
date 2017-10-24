2c23eb48a3f668ded0052414bfd76d4df021bdd9
Kathrin Koebel <kathrin.koebel@students.fhnw.ch>
2017-10-25 00:47:13 +0200

    Drawer nav experiment

---
+++ MatApp/app/src/main/java/com/matapp/matapp/fragment/UsersFragment.java
@@ -0,0 +1,10 @@
+package com.matapp.matapp.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by kathrinkoebel on 24.10.17.
+ */
+
+public class UsersFragment extends Fragment {
+}
