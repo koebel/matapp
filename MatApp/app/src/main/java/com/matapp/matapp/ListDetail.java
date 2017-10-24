31fd8f26c576cf367325ea9ceab84f6ac360168b
Kathrin Koebel <kathrin.koebel@students.fhnw.ch>
2017-10-25 00:48:13 +0200

    Revert "Drawer nav experiment"
    This reverts commit 2c23eb48a3f668ded0052414bfd76d4df021bdd9.

--- MatApp/app/src/main/java/com/matapp/matapp/ListDetail.java
+++ MatApp/app/src/main/java/com/matapp/matapp/ListDetail.java
@@ -14,8 +14,8 @@ public class ListDetail extends AppCompatActivity {
 
 
         // Get the Intent that started this activity and extract the string
-        // Intent intent = getIntent();
-        // String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
         // TextView textView = (TextView) findViewById(R.id.textView);
