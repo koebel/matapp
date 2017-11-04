6ec98f4807ef590738a39fbb68cb22d7a4adada7
Kathrin Koebel <kathrin.koebel@students.fhnw.ch>
2017-11-04 13:17:26 +0000

    Getter & Setter added for Material

--- MatApp/app/src/main/java/com/matapp/matapp/other/Material.java
+++ MatApp/app/src/main/java/com/matapp/matapp/other/Material.java
@@ -18,7 +18,7 @@ public class Material {
     public String loan_until;
     public String loan_note;
 
-
+    /* Constructor */
     public Material(String title, String description){
         this.title = title;
         this.description = description;
@@ -29,4 +29,102 @@ public class Material {
         this.description = description;
         this.owner = owner;
     }
+
+    /* Getter & Setter */
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getGps() {
+        return gps;
+    }
+
+    public void setGps(String gps) {
+        this.gps = gps;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getLoanName() {
+        return loan_name;
+    }
+
+    public void setLoanName(String loan_name) {
+        this.loan_name = loan_name;
+    }
+
+    public String getLoanContact() {
+        return loan_contact;
+    }
+
+    public void setLoanContact(String loan_contact) {
+        this.loan_contact = loan_contact;
+    }
+
+    public String getLoanUntil() {
+        return loan_until;
+    }
+
+    public void setLoanUntil(String loan_until) {
+        this.loan_until = loan_until;
+    }
+
+    public String getLoanNote() {
+        return loan_note;
+    }
+
+    public void setLoanNote(String loan_note) {
+        this.loan_note = loan_note;
+    }
+
 }
