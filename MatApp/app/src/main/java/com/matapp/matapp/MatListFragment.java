b887ba1b3399b9d76ca4189bcc14a3fed89429b5
Kathrin Koebel <kathrin.koebel@students.fhnw.ch>
2017-11-09 12:22:36 +0100

    prepare for thumb image

--- MatApp/app/src/main/java/com/matapp/matapp/MatListFragment.java
+++ MatApp/app/src/main/java/com/matapp/matapp/MatListFragment.java
@@ -2,28 +2,17 @@ package com.matapp.matapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 
-import com.matapp.matapp.MatAddActivity;
-import com.matapp.matapp.RecyclerAdapter;
-import com.matapp.matapp.R;
 import com.matapp.matapp.other.Material;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +25,7 @@ import java.util.List;
  *
  */
 
-public class MatListActivity extends Fragment {
+public class MatListFragment extends Fragment {
 
     public List<Material> materials;
 
@@ -52,7 +41,7 @@ public class MatListActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
-        View view = inflater.inflate(R.layout.activity_matlist, parent, false);
+        View view = inflater.inflate(R.layout.fragment_mat_list, parent, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.rv_matlist);
         initializeData();
